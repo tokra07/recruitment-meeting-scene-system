cb7917b5ad2697fe7b920fa2e7bd4362bf09b9e3
Larina <dongxing@live.cn>
2024-01-28 15:25:34 +0800

    底部消息滚动

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,8 +13,10 @@
   </el-row>
   <el-row>
     <el-col>
-      <div style="margin-top: 25px;margin-left: 5%;margin-right: 5%;">
-        {{ messageText }}
+      <div class="message-out">
+        <div class="messages">
+          <span>{{ messageText }}</span>
+        </div>
       </div>
     </el-col>
   </el-row>
@@ -43,7 +45,64 @@ export default {
   mounted () {
     getInfo().then((res) => {
       console.log(res)
+      for (let i = 0; i < res.data.length; i++) {
+        this.messageText += res.data[i].title + '    '
+      }
     })
   }
 }
 </script>
+
+<style>
+.message-out{
+  width: 90%;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  margin-top: 20px;
+  margin-left: 5%;
+  margin-right: 5%;
+  overflow: hidden;
+}
+.messages{
+  font-size: 2rem;
+  white-space: normal;
+  width: max-content;
+  animation: marquee 20s infinite;
+}
+
+@keyframes marquee {
+  0% {
+    transform: translateX(0);
+  }
+  10% {
+    transform: translateX(0);
+  }
+  20% {
+    transform: translateX(-20%);
+  }
+  30% {
+    transform: translateX(-20%);
+  }
+  40% {
+    transform: translateX(-40%);
+  }
+  50% {
+    transform: translateX(-40%);
+  }
+  60% {
+    transform: translateX(-60%);
+  }
+  70% {
+    transform: translateX(-60%);
+  }
+  80% {
+    transform: translateX(-80%);
+  }
+  90% {
+    transform: translateX(-80%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
