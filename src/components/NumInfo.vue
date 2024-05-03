2a75949d2466b339cd81ed32344bcaf3167f0d57
larina <dongxing@live.cn>
2024-05-03 20:33:51 +0800

    更新数据显示

--- src/components/NumInfo.vue
+++ src/components/NumInfo.vue
@@ -1,10 +1,13 @@
 <template>
-    <div style="margin-top: 12%;"><span class="main-info">当前企业数：</span><Flipper style="margin-top: 1px;" :val=epNum suffix="家"></Flipper></div>
+    <div style="margin-top: 12%;"><span class="main-info">当前投递简历数：</span><Flipper :val=resumeNum suffix="份"></Flipper></div>
     <div><span class="main-info">当前展厅人数：</span><Flipper :val=personMum suffix="人次"></Flipper></div>
-    <div><span class="main-info">当前投递简历数：</span><Flipper :val=resumeNum suffix="份"></Flipper></div>
 </template>
 
 <script>
+import { getBoothList } from '@/api/api'
+import { lightControls } from '@/model/lightControls'
+import { bulbControls } from '@/model/bulbControls'
+import { addTag } from '@/model/cssControls'
 import Flipper from '@/components/FlipperView.vue'
 export default {
   name: 'NumInfo',
@@ -13,7 +16,9 @@ export default {
   },
   data () {
     return {
-      epNum: 0,
+      yyy: 12,
+      mmm: 10,
+      ddd: 12,
       resumeNum: 0,
       personMum: 0,
       resumeSocket: '',
@@ -21,16 +26,62 @@ export default {
     }
   },
   created () {
-
+    this.resume()
+    this.people()
   },
   unmounted () {
-
+    this.resumeSocket.close()
+    this.peopleSocket.close()
   },
   mounted () {
 
   },
   methods: {
-
+    resume () {
+      const _this = this
+      _this.resumeSocket = new WebSocket('ws://jy.chifengrencai.com//ledapi/getDeliveriesNumber')
+      _this.resumeSocket.addEventListener('message', function (event) {
+        console.log('getDeliveriesNumber', event)
+        if (event.data === '连接成功') {
+          _this.resumeSocket.send(1)
+        } else {
+          const comList = {}
+          getBoothList().then((res) => {
+            console.log('公司数据', res.data)
+            for (let i = 0; i < res.data.length; i++) {
+              comList[res.data[i].boothNo] = res.data[i].companyName
+            }
+            console.log('comList', comList)
+            const deliverNum = JSON.parse(event.data)
+            const deliverKeys = Object.keys(deliverNum)
+            console.log('返回的值', comList[deliverKeys])
+            const boothNum = 'spotLight' + deliverKeys
+            const bulbName = 'bulb' + deliverKeys
+            const cssNum = 'tag' + deliverKeys
+            lightControls(boothNum)
+            bulbControls(bulbName)
+            addTag(cssNum)
+            _this.resumeNum++
+            // _this.$message({
+            //   message: '恭喜' + comList[deliverKeys] + '收到一份简历',
+            //   type: 'success'
+            // })
+          })
+        }
+      })
+    },
+    people () {
+      const _this = this
+      _this.peopleSocket = new WebSocket('ws://jy.chifengrencai.com//ledapi/getCurrentPersonage')
+      _this.peopleSocket.addEventListener('message', function (event) {
+        console.log('getCurrentPersonage', event)
+        if (event.data === '连接成功') {
+          _this.peopleSocket.send(1)
+        } else {
+          _this.personMum++
+        }
+      })
+    }
   }
 }
 </script>
--- src/components/NumInfos.vue
+++ src/components/NumInfos.vue
@@ -1,14 +1,10 @@
 <template>
   <div style="margin-top: 12%;"><span class="main-info">当前企业数：</span><Flipper style="margin-top: 1px;" :val=epNum suffix="家"></Flipper></div>
-  <div><span class="main-info">当前展厅人数：</span><Flipper :val=personMum suffix="人次"></Flipper></div>
-  <div><span class="main-info">当前投递简历数：</span><Flipper :val=resumeNum suffix="份"></Flipper></div>
+  <div><span class="main-info">当前岗位数：</span><Flipper :val=posNum suffix="个"></Flipper></div>
 </template>
 
 <script>
-import { getBoothList } from '@/api/api'
-import { lightControls } from '@/model/lightControls'
-import { bulbControls } from '@/model/bulbControls'
-import { addTag } from '@/model/cssControls'
+import { getBoothList, getJobIndustry } from '@/api/api'
 import Flipper from '@/components/FlipperView.vue'
 export default {
   name: 'NumInfos',
@@ -18,71 +14,24 @@ export default {
   data () {
     return {
       epNum: 0,
-      resumeNum: 0,
-      personMum: 0,
+      posNum: 0,
       resumeSocket: '',
       peopleSocket: ''
     }
-  },
-  created () {
-    this.resume()
-    this.people()
-  },
-  unmounted () {
-    this.resumeSocket.close()
-    this.peopleSocket.close()
   },
   mounted () {
     getBoothList().then((res) => {
       this.epNum = res.data.length
     })
+    getJobIndustry().then((res) => {
+      console.log('获取岗位数', res)
+      for (let i = 0; i < res.data.length; i++) {
+        this.posNum += res.data[i].numbers
+      }
+    })
   },
   methods: {
-    resume () {
-      const _this = this
-      _this.resumeSocket = new WebSocket('ws://jy.chifengrencai.com//ledapi/getDeliveriesNumber')
-      _this.resumeSocket.addEventListener('message', function (event) {
-        console.log('getDeliveriesNumber', event)
-        if (event.data === '连接成功') {
-          _this.resumeSocket.send(1)
-        } else {
-          const comList = {}
-          getBoothList().then((res) => {
-            console.log('公司数据', res.data)
-            for (let i = 0; i < res.data.length; i++) {
-              comList[res.data[i].boothNo] = res.data[i].companyName
-            }
-            console.log('comList', comList)
-            const deliverNum = JSON.parse(event.data)
-            const deliverKeys = Object.keys(deliverNum)
-            console.log('返回的值', comList[deliverKeys])
-            const boothNum = 'spotLight' + deliverKeys
-            const bulbName = 'bulb' + deliverKeys
-            const cssNum = 'tag' + deliverKeys
-            lightControls(boothNum)
-            bulbControls(bulbName)
-            addTag(cssNum)
-            _this.resumeNum++
-            // _this.$message({
-            //   message: '恭喜' + comList[deliverKeys] + '收到一份简历',
-            //   type: 'success'
-            // })
-          })
-        }
-      })
-    },
-    people () {
-      const _this = this
-      _this.peopleSocket = new WebSocket('ws://jy.chifengrencai.com//ledapi/getCurrentPersonage')
-      _this.peopleSocket.addEventListener('message', function (event) {
-        console.log('getCurrentPersonage', event)
-        if (event.data === '连接成功') {
-          _this.peopleSocket.send(1)
-        } else {
-          _this.personMum++
-        }
-      })
-    }
+
   }
 }
 </script>
